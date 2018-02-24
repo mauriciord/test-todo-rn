dfda0d70bf22b1cc07f453157d2a691fe75f6f7d
Maurício R Duarte <mauriciord@me.com>
2018-02-24 08:30:45 -0300

    fixed

---
+++ main.js
@@ -0,0 +1,18 @@
+import Expo from 'expo';
+import React from 'react'
+import { AppRegistry } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import reducer from './src/redux-flow/reducers/index'
+import App from './App';
+
+const store = createStore(reducer)
+
+const renderApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+Expo.registerRootComponent(renderApp);
